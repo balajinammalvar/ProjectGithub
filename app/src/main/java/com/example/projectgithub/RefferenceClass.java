84cdb11e1efeaf688cfae83cda53cd6177635c53
SYSTEM02 <balajianb@gmail.com>
2019-12-26 15:18:26 +0530

    Background Service and Local Database

--- app/src/main/java/com/example/projectgithub/RefferenceClass.java
+++ app/src/main/java/com/example/projectgithub/RefferenceClass.java
@@ -2,13 +2,19 @@ package com.example.projectgithub;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.util.Log;
 
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.io.File;
+
 import alertbox.Alertbox;
 import api.APIService;
 import api.RetrofitClient;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -58,6 +64,50 @@ public class RefferenceClass {
 //		home.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
 //		Alertbox alert=new Alertbox(RefferenceClass.this);
 //		alert.sucess(s,home);
+//	}
+
+	//upload images method
+//	private void uploadimage(File selectimage) {
+//		try {
+//			loading = new ProgressDialog(ScanActivity.this);
+//			loading.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//			loading.setCancelable(false);
+//			loading.setMessage("Loading...");
+//			if (!loading.isShowing())
+//				loading.show();
+//			APIService service = RetrofitClient.getApiUplaodService();
+//			String type="image/png";
+//			imagename=selectimage.getName();
+//			RequestBody requestBody = RequestBody.create(MediaType.parse(type), selectimage);
+//			MultipartBody.Part filePart = MultipartBody.Part.createFormData("image", selectimage.getName(), requestBody);
+//			final Call<UploadImage> request = service.imageupload(filePart);
+//			request.enqueue(new Callback<UploadImage>() {
+//				@Override
+//				public void onResponse(Call<UploadImage> call, Response<UploadImage> response) {
+//					if (response.body().getMessage().equals("Success.")) {
+//						loading.dismiss();
+//						uploaduserdetails();
+//					}
+////                                else {
+////                                    Nointernet("Please Try Again later");
+////                                }
+//				}
+//				@Override
+//				public void onFailure(Call<UploadImage> call, Throwable t) {
+//					Log.v("Upload Exception", t.getMessage());
+//					loading.dismiss();
+//					t.printStackTrace();
+//
+//				}
+//
+//			});
+//		}
+//		catch (Exception ex) {
+//			if (loading.isShowing())
+//				loading.dismiss();
+//			Log.v("Exception", ex.getMessage());
+//
+//		}
 //	}
 
 }
