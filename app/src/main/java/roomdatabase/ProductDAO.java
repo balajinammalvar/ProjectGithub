eb8534339d12f0c82faf71eaa4332515ccc7e079
SYSTEM02 <balajianb@gmail.com>
2020-02-19 14:59:50 +0530

    room updated

--- app/src/main/java/roomdatabase/ProductDAO.java
+++ app/src/main/java/roomdatabase/ProductDAO.java
@@ -1,8 +1,13 @@
 package roomdatabase;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 
 @Dao
 public interface ProductDAO {
+
+
+    @Insert
+    void insertusername(Users users);
 
 }
--- app/src/main/java/roomdatabase/Users.java
+++ app/src/main/java/roomdatabase/Users.java
@@ -4,6 +4,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.gson.annotations.SerializedName;
+
 @Entity (tableName = "Users")
 public class Users {
 
@@ -11,7 +13,20 @@ public class Users {
     public int id;
 
     @ColumnInfo(name = "UserName")
+    @SerializedName("UserName")
     private String UserName;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @ColumnInfo(name = "password")
+    @SerializedName("password")
+    private String password;
 
     public int getId() {
         return id;
